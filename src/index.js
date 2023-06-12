871808282b02913157538d00cda944eb69056827
Royer <rojorom7@gmail.com>
2023-06-12 14:23:48 -0500

    db string conecion. script update db etc

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import app from "./app";
 import "./utils/mongoose";
+import { PORT } from "./config";
 
 app.listen(app.get("port"));
-console.log(`server on port ${app.get("port")}`);
+console.log(`server on port ${app.get("port")}`); 
