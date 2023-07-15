d16a91263fc886ebccc3effe6580a6f97506b45c
Royer <rojorom7@gmail.com>
2023-07-15 13:02:12 -0500

    add required

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import app from "./app";
 import "./utils/mongoose";
 import { PORT } from "./config";
+require('text-encoding-utf-8');
+
 
 app.listen(app.get("port"));
 console.log(`server on port ${app.get("port")}`); 
